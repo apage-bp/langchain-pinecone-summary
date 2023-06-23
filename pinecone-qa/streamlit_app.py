02bb058f510b16f72b58ab3a6b3a9ce634a0772f
Alex Page <apage@bigpanda.io>
2023-06-22 20:39:22 -0400

    adjusting query code

--- pinecone-qa/streamlit_app.py
+++ pinecone-qa/streamlit_app.py
@@ -16,11 +16,10 @@ with st.sidebar:
     pinecone_env = st.text_input("Pinecone environment")
     pinecone_index = st.text_input("Pinecone index name")
 source_doc = st.file_uploader("Upload source document", type="pdf", label_visibility="collapsed")
-query = st.text_input("Enter your query")
 
 if st.button("Submit File"):
     # Validate inputs
-    if not openai_api_key or not pinecone_api_key or not pinecone_env or not pinecone_index or not source_doc or not query:
+    if not openai_api_key or not pinecone_api_key or not pinecone_env or not pinecone_index or not source_doc:
         st.warning(f"Please upload the document and provide the missing fields.")
     else:
         try:
@@ -41,12 +40,16 @@ if st.button("Submit File"):
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
+query = st.text_input("Enter your query")
 if st.button("Run Query"):
-    try:
-        # Initialize the OpenAI module, load and run the Retrieval Q&A chain
-        llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-        qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=retriever)
-        response = qa.run(query)
-        st.success(response)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    if not query:
+        st.warning(f"Please enter a query.")
+    else:
+        try:
+            # Initialize the OpenAI module, load and run the Retrieval Q&A chain
+            llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+            qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=retriever)
+            response = qa.run(query)
+            st.success(response)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
